e3508eb38c6e160c07a55344d0ed4cafeb7dbd64
Alex Whedbee <awhedbee22@gmail.com>
2024-09-02 11:49:10 -0700

    supabase configs

--- config/supabase.ts
+++ config/supabase.ts
@@ -1,5 +1,4 @@
 import "react-native-url-polyfill/auto";
-
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
@@ -10,4 +9,11 @@ if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables');
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
